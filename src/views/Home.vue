1468e72f2bc4c8d24238b53c582543444db31723
Chris Ngo <cngo2417@gmail.com>
2018-08-15 21:00:07 -0500

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h2>{{ testMessage }}</h2>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      testMessage: "This is a test."
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+</script>
