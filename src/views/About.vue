941d522c37dea648e597df7fdd92815705bb375b
Chris Ngo <cngo2417@gmail.com>
2018-08-25 02:09:01 -0500

    Refined layout blueprint starting from title, subtitle, pages-links, and search-bar. Edited & separated search results layout but still work-in-progress. Need to incorporate everything from search-bar and up into an enlarged navbar.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>About Capstone</h1>
-    <p>We believe that language is beautiful and malleable(mostly). It is clear to us that as new technology and new generations come along, they create their own ways to communicate. We think it is very important to preserve the foundations of communication while still keeping up with the rapidity of modern times. So we built this resource to help the world document it's many uses of language and styles of communicating</p> 
+    <p>We believe that language is beautiful and malleable(mostly). As new technology and new generations come along, they create their own ways to communicate. We think it is very important to preserve the foundations of communication while still keeping up with the rapidity of modern times. So we built this resource to help the world document it's many uses of language and styles of communicating</p> 
 
     <p>Because saying "Back in my day..." should no longer be followed by a condescending comment. It should be followed up with a story, some history and context. Today is still your day. Don't stay in the past, stay relevant!</p>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,108 @@
 <template>
   <div class="home">
     <div>
-      <h1>Welcome to Capstone!</h1>
-        <h6>Document, create, discuss & learn about the history and current evolution of language & communication.</h6>
+      <h1 class="title">CONVEY</h1>
     </div>
-
-    <div class="search">
-      <div>
-        <h4>Start here</h4>
-        <input type="text" v-model="searchBar">
-        <button v-on:click="searchEntry(word)">Search</button>
+ 
+    <div class="subtitle">
+      <h6>It's time we have better conversations.</h6>
+    </div>
+      <div class="pages-links">
+        <h6> [ Dictionary ] . [ Thesaurus ] . [ Urban Dictionary ] . [ Origin & History ] . [ Create ] . [ Blog ] . [ Vote/Discuss ] </h6>
       </div>
-    </div>
+        <div class="search-bar">
+          <h3>search <input type="text" v-model="search"></h3>
+          <button v-on:click="searchEntry()">Convey It!</button>
+        </div>
+
+
+    <div class="results-body">
+      <h5>Your search results: {{ search }}</h5> 
+        <p v-if="words.length === 0">The word you are looking for does not yet exist with Convey.</p>
+        <ul>
+          <li v-for="word in words">
+            <p class="word"><h3>{{ word.word }}</h3></p>
+            <p class="definition">Definition: <h5>{{ word.definition }}</h5></p>
+            <p class="user-name">By User: <h6>{{ word.user_id }}</h6></p>
+          </li>
+        </ul> 
+    </div> 
   </div>
 </template>
 
 <style>
+.title {
+  font-size: 45px;
+  font-weight: bold;
+  color: black;
+  text-align: center;
+}
+.subtitle {
+  font-variant: small-caps;
+  color: black;
+  text-align: center;
+  padding-bottom: 40px;
+}
+.pages-links {
+  font-size: 20;
+  color: black;
+  text-align: center;
+  padding-bottom: 60px;
+}
+.search-bar {
+  color: black;
+  text-align: center;
+  padding-top: 20px;
+}
+.results-body {
+  text-align: left;
+  padding-top: 50px;
+  padding-left: 100px;
+  padding-right: 100px;
+  padding-bottom: 50px;
+}
+.word {
+  font-weight: bold;
+  font-size: 22px;
+  padding-top: 30px;
+  padding-left: 7px;
+}
+.definition {
+  padding-top: 7px;
+  padding-left: 7px;
+}
+.user-name {
+  padding-top: 7px;
+  padding-left: 7px;
+}
 </style>
 
 <script>
+var axios = require("axios");
+
 export default {
   data: function() {
     return {
-      searchBar: "",
-      searchEntry: "Route SHOW search entry to Wordnik API for data."
+      words: [{ id: "", word: "", definition: "", user_id: "" }],
+      search: "",
+      message: "Chris was here"
     };
   },
-  created: function() {},
+  created: function() {
+    console.log("Finding your word.");
+  },
   methods: {
-    searchEntry: function(word) {
-
+    searchEntry: function() {
+      var params = {
+        search: this.search
+      };
+      console.log(params);
+      axios.get("http://localhost:3000/api/words", {params: params}).then(
+        function(response) {
+          console.table(response.data);
+          this.words = response.data;
+        }.bind(this)
+      );
     }
   },
   computed: {}
