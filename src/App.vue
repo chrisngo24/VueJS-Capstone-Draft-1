74c9172ac27aa0f1cae48bcd82a0605697049239
Chris Ngo <cngo2417@gmail.com>
2018-09-02 12:14:39 -0500

    Setting up Navbar and search bar within theme. Results body is broken.

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,182 @@
 <!-- PARENT COMPONENT -->
 <template>
   <div id="app">
-     <!--  -->
+     <!-- LOGO AS CLICKABLE HOMEPAGE LINK -->
     <header class="header">
       <nav class="navbar navbar-expand-lg">
         <div class="container"><a href="index.html" class="navbar-brand"><img src="images/conveyNewWhiteBrdr.png" alt="logo" height="65" width="210" class="d-none d-lg-block"><img src="images/conveyNewWhiteBrdr.png" alt="logo" height="65" width="210" class="d-block d-lg-none"><span class="sr-only">Go to homepage</span></a>
-          <!-- <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right">Menu<i class="fa fa-bars"></i></button> -->
 
           <div id="navbarSupportedContent" class="collapse navbar-collapse">
             <ul class="navbar-nav ml-auto">
-              <li class="nav-item active"><a href="/#/" class="nav-link">Home <span class="sr-only">(current)</span></a></li>
-              <li class="nav-item"><a href="/#/about" class="nav-link">About Us</a></li>
-              <li class="nav-item dropdown"><a id="community" href="/#/signup" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Conveyers</a>
-                <div aria-labelledby="community" class="dropdown-menu"><a href="/#/login" class="dropdown-item">Login</a><a href="/#/signup" class="dropdown-item">Signup</a><!-- <a href="client-applicants.html" class="dropdown-item">Applicants                </a><a href="client-job.html" class="dropdown-item">Post a new job  </a> --></div>
+
+              <li class="nav-item"><a href="/#/createWords" class="nav-link">Create</a></li>
+
+              <!-- DROPDOWN MENU -->
+              <li class="nav-item dropdown"><a id="community" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Menu</a>
+                <div aria-labelledby="community" class="dropdown-menu"><a href="/#/about" class="dropdown-item">About Us</a><a href="/#/signup" class="dropdown-item">Signup</a><a href="" class="dropdown-item">Explore</a><a href="" class="dropdown-item">Discuss</a><a href="" class="dropdown-item">Vote</a><a href="" class="dropdown-item">Community</a><a href="/#/signup" class="dropdown-item">Blog</a><a href="" class="dropdown-item">Library</a></div>
               </li>
 
-             <!--  <li class="nav-item dropdown"><a id="pages" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Pages</a>
-                <div aria-labelledby="pages" class="dropdown-menu"><a href="index.html" class="dropdown-item">Home</a><a href="category.html" class="dropdown-item">Job Listing</a><a href="detail.html" class="dropdown-item">Job Detail                </a><a href="about.html" class="dropdown-item">About                </a><a href="contact.html" class="dropdown-item">Contact                </a><a href="blog.html" class="dropdown-item">Blog Listing                </a><a href="post.html" class="dropdown-item">Blog Post                </a><a href="packages.html" class="dropdown-item">Pricing packages    </a><a href="testimonials.html" class="dropdown-item">Testimonials    </a><a href="text.html" class="dropdown-item">Text Page    </a><a href="faq.html" class="dropdown-item">FAQ</a></div>
-              </li> -->
+              <!-- LOGIN MODAL -->
               <li class="nav-item dropdown"><a href="#loginModal" data-toggle="modal" class="btn navbar-btn btn-outline-light mb-5 mb-lg-0"> <i class="fa fa-sign-in"></i>Login</a></li>
             </ul>
           </div>
         </div>
       </nav>
     </header>
-
-    <!--   <div>
-        <h1 class="title">Navstone</h1>
-      </div>
-      <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-        <div class="btn-group mr-2" role="group" aria-label="First group">
-          <button type="button" class="btn-nav"><a href="/#/">Home</a></button>
-          <button type="button" class="btn-nav"><a href="/#/about">About</a></button>
-          <button type="button" class="btn-nav">Account</button>
-          <button type="button" class="btn-nav"><a href="/#/signup">Signup</a></button>
-          <button type="button" class="btn-nav"><a href="/#/login">Login</a></button>
-          <button type="button" class="btn-nav"><a href="/#/logout">Logout</a></button>
-        </div>
-      </div> -->
-
     <router-view/>
-
-    <footer class="footer">
-      <div class="footer__block">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-4 col-md-12 mb-5">
-              <h4 class="h5">About Jobs</h4>
-              <p>Jobs is a <strong>Bootstrap 3 jobs board theme </strong> developed in the middle of Europe by <a href="http://ondrejsvestka.cz/">Ondrej Svestka</a></p>
-              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. </p>
+      <footer class="footer">
+        <div class="footer__block">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-4 col-md-12 mb-5">
+                <h4 class="h5">About Jobs</h4>
+                <p>Jobs is a <strong>Bootstrap 3 jobs board theme </strong> developed in the middle of Europe by <a href="http://ondrejsvestka.cz/">Ondrej Svestka</a></p>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. </p>
+              </div>
+              <div class="col-lg-4 col-md-6 mb-5">
+                <h4 class="h5">This Theme</h4>
+                <ul class="list-unstyled">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="category.html">Jobs Listing</a></li>
+                  <li><a href="detail.html">Job detail</a></li>
+                  <li><a href="about.html">About</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                  <li><a href="blog.html">Blog listing</a></li>
+                  <li><a href="post.html">Blog Post</a></li>
+                  <li><a href="packages.html">Pricing packages</a></li>
+                  <li><a href="testimonials.html">Testimonials</a></li>
+                  <li><a href="text.html">Text page</a></li>
+                  <li><a href="faq.html">FAQ</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-4 col-md-6">
+                <h4 class="h5">Our offices</h4>
+                <h5>London</h5>
+                <p> Donec eleifend<br />libero at sagittis<br />mollis + 44 2556 789 8799</p>
+                <h5>Paris</h5>
+                <p> Donec eleifend<br />libero at sagittis<br />mollis + 41 2556 789 8799</p>
+              </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5">
-              <h4 class="h5">This Theme</h4>
-              <ul class="list-unstyled">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="category.html">Jobs Listing</a></li>
-                <li><a href="detail.html">Job detail</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="blog.html">Blog listing</a></li>
-                <li><a href="post.html">Blog Post</a></li>
-                <li><a href="packages.html">Pricing packages</a></li>
-                <li><a href="testimonials.html">Testimonials</a></li>
-                <li><a href="text.html">Text page</a></li>
-                <li><a href="faq.html">FAQ</a></li>
-              </ul>
-            </div>
-            <div class="col-lg-4 col-md-6">
-              <h4 class="h5">Our offices</h4>
-              <h5>London</h5>
-              <p> Donec eleifend<br />libero at sagittis<br />mollis + 44 2556 789 8799</p>
-              <h5>Paris</h5>
-              <p> Donec eleifend<br />libero at sagittis<br />mollis + 41 2556 789 8799</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-4 col-md-6 mb-5">
-              <h4 class="h5">Client zone</h4>
-              <ul class="list-unstyled">
-                <li><a href="client-register.html">Login or Register</a></li>
-                <li><a href="client-dashboard.html">Dashboard</a></li>
-                <li><a href="client-applicants.html">Applicants</a></li>
-                <li><a href="client-job.html">Post a new job</a></li>
-              </ul>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-5">
-              <h4 class="h5">Let's be Friends</h4>
-              <p class="social"><a href="#" data-animate-hover="pulse" class="external facebook"><i class="fa fa-facebook"></i></a><a href="#" data-animate-hover="pulse" class="external gplus"><i class="fa fa-google-plus"></i></a><a href="#" data-animate-hover="pulse" class="external twitter"><i class="fa fa-twitter"></i></a><a href="#" data-animate-hover="pulse" class="email"><i class="fa fa-envelope"></i></a></p>
-            </div>
-            <div class="col-lg-4 col-md-12 mb-5">
-              <h4 class="h5">News and Updates</h4>
-              <p>Sign up to get weekly portion of fresh jobs and news from us.</p>
-              <form class="footer__newsletter">
-                <div class="input-group">
-                  <input type="text" placeholder="Enter your email address" aria-describedby="emailAddress" class="form-control">
-                  <div class="input-group-append">
-                    <button id="emailAddress" type="button" class="btn btn-default input-group-text"><i class="fa fa-send"></i></button>
+            <div class="row">
+              <div class="col-lg-4 col-md-6 mb-5">
+                <h4 class="h5">Client zone</h4>
+                <ul class="list-unstyled">
+                  <li><a href="client-register.html">Login or Register</a></li>
+                  <li><a href="client-dashboard.html">Dashboard</a></li>
+                  <li><a href="client-applicants.html">Applicants</a></li>
+                  <li><a href="client-job.html">Post a new job</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-4 col-md-6 mb-5">
+                <h4 class="h5">Let's be Friends</h4>
+                <p class="social"><a href="#" data-animate-hover="pulse" class="external facebook"><i class="fa fa-facebook"></i></a><a href="#" data-animate-hover="pulse" class="external gplus"><i class="fa fa-google-plus"></i></a><a href="#" data-animate-hover="pulse" class="external twitter"><i class="fa fa-twitter"></i></a><a href="#" data-animate-hover="pulse" class="email"><i class="fa fa-envelope"></i></a></p>
+              </div>
+              <div class="col-lg-4 col-md-12 mb-5">
+                <h4 class="h5">News and Updates</h4>
+                <p>Sign up to get weekly portion of fresh jobs and news from us.</p>
+                <form class="footer__newsletter">
+                  <div class="input-group">
+                    <input type="text" placeholder="Enter your email address" aria-describedby="emailAddress" class="form-control">
+                    <div class="input-group-append">
+                      <button id="emailAddress" type="button" class="btn btn-default input-group-text"><i class="fa fa-send"></i></button>
+                    </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="footer__copyright">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 text-md-left text-center">
-              <p>&copy;2017 Best company</p>
-            </div>
-            <div class="col-md-6 text-md-right text-center">
-              <p class="credit">Theme by <a href="https://ondrejsvestka.cz/">Ondrej Svestka</a></p>
+        <div class="footer__copyright">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6 text-md-left text-center">
+                <p>&copy;2017 Best company</p>
+              </div>
+              <div class="col-md-6 text-md-right text-center">
+                <p class="credit">Theme by <a href="https://ondrejsvestka.cz/">Ondrej Svestka</a></p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
   </div>
 </template>
 
+<body>
+  <!-- SEARCH BOX -->
+  <section class="job-form-section job-form-section--image">
+
+  </section>
+</body>
+
 <style>
+
 </style>
+
+<script>
+import CommentsModal from "../components/CommentsModal.vue";
+import DefinitionsModal from "../components/DefinitionsModal.vue";
+var axios = require("axios");
+
+export default {
+  data: function() {
+    return {
+      words: [],
+      wordnikWords: [],
+      search: "",
+      i: 0
+    };
+  },
+  components: {
+    commentsModal: CommentsModal,
+    definitionsModal: DefinitionsModal
+  },
+  created: function() {
+    console.log("Finding your word.");
+  },
+  methods: {
+    searchEntry: function() {
+      var params = {
+        search: this.search
+      };
+      console.log(params);
+      axios.get("http://localhost:3000/api/words", { params: params }).then(
+        function(response) {
+          console.table(response.data);
+          this.words = response.data.custom_words;
+          console.log("CONVEY WORDS", this.words);
+        }.bind(this)
+      );
+      axios.get("http://localhost:3000/api/wordnik", { params: params }).then(
+        function(response) {
+          // The response.data is an array of hashes
+          console.log("WORDNIK WORDS", response.data);
+          this.wordnikWords = response.data;
+        }.bind(this)
+      );
+    },
+    trimWords: function(wordnikWords) {
+      return wordnikWords.slice(0, 3);
+    },
+    createDefinition: function(word) {
+      var params = {
+        definition: word.newDefinition,
+        word_id: word.id
+      };
+      axios
+        .post("http://localhost:3000/api/definitions", params)
+        .then(function(response) {
+          var newDefinition = response.data;
+          console.log(newDefinition);
+          word.definitions.push(newDefinition);
+          word.newDefinition = "";
+        });
+    },
+    searchTag: function(tag) {
+      this.search = tag.name;
+      this.searchEntry();
+    }
+  },
+  computed: {}
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,13 +8,6 @@
       <h6>It's time we have better conversations.</h6>
     </div>
 
-      <div class="pages-links">
-        <button type="button" class="btn-pages-links-tab">Dictionary</button>
-        <button type="button" class="btn-pages-links-tab">Thesaurus</button>
-        <button type="button" class="btn-nav"><a href="/#/createWords">CREATE</a></button><button type="button" class="btn-pages-links-tab">Discuss</button>
-        <button type="button" class="btn-pages-links-tab">Explore</button>
-        <button type="button" class="btn-pages-links-tab">Vote</button>
-      </div>
       <form class="form-center">
         <input class="search-form" type="search" placeholder="   Search" aria-label="Search" v-model="search">
         <button class="search-button" v-on:click="searchEntry()" type="submit">Search</button>
@@ -69,89 +62,89 @@
 </template>
 
 <style>
-  .title {
-    font-size: 45px;
-    font-weight: bold;
-    color: black;
-    text-align: center;
-  }
-  .subtitle {
-    font-variant: small-caps;
-    color: black;
-    text-align: center;
-    padding-bottom: 15px;
-  }
-  .pages-links {
-    font-size: 20;
-    color: black;
-    text-align: center;
-    padding-bottom: 70px;
-  }
-  .btn-pages-links-tab {
-    border-top: 0px white;
-    border-right: 1px solid black;
-    border-bottom: 0px white;
-    border-left: 1px solid black;
-    background-color: white;
-  }
-  .form-inline {
-    color: black;
-    text-align: center;
-    padding-top: 30px;
-  }
-  .search-form {
-    height: 39px;
-    width: 38%;
-    border-top: 0px white;
-    border-right: 0px white;
-    border-bottom: 1.5px solid black;
-    border-left: 0px white;
-    background-color: white;
-    border-radius: 11px;
-    margin-bottom: 4px;
-  }
-  .search-button {
-    padding-top: 3px;
-    height: 39px;
-    width: 9%;
-    background-color: white;
-    color: black;
-    font-size: 20px;
-    font-weight: bold;
-    border-top: 1.5px solid black;
-    border-right: 0px white;
-    border-bottom: 0px white;
-    border-left: 0px white;
-    border-style: solid;
-    border-radius: 11px;
-    /*box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 5px 0 rgba(0, 0, 0, 0.19);*/
-  }
-  .search-button:hover {
-    background-color: black;
-    color: white;
-  }
-  .results-body {
-    text-align: left;
-    padding-top: 50px;
-    padding-left: 100px;
-    padding-right: 100px;
-    padding-bottom: 50px;
-  }
-  .word {
-    font-weight: bold;
-    font-style: italic;
-    font-size: 22px;
-    padding-top: 30px;
-    padding-left: 7px;
-  }
-  .definition {
-    padding-top: 7px;
-    padding-left: 7px;
-  }
-  .user-name {
-    padding-top: 7px;
-    padding-left: 7px;
-  }
+.title {
+  font-size: 45px;
+  font-weight: bold;
+  color: black;
+  text-align: center;
+}
+.subtitle {
+  font-variant: small-caps;
+  color: black;
+  text-align: center;
+  padding-bottom: 15px;
+}
+.pages-links {
+  font-size: 20;
+  color: black;
+  text-align: center;
+  padding-bottom: 70px;
+}
+.btn-pages-links-tab {
+  border-top: 0px white;
+  border-right: 1px solid black;
+  border-bottom: 0px white;
+  border-left: 1px solid black;
+  background-color: white;
+}
+.form-inline {
+  color: black;
+  text-align: center;
+  padding-top: 30px;
+}
+.search-form {
+  height: 39px;
+  width: 38%;
+  border-top: 0px white;
+  border-right: 0px white;
+  border-bottom: 1.5px solid black;
+  border-left: 0px white;
+  background-color: white;
+  border-radius: 11px;
+  margin-bottom: 4px;
+}
+.search-button {
+  padding-top: 3px;
+  height: 39px;
+  width: 9%;
+  background-color: white;
+  color: black;
+  font-size: 20px;
+  font-weight: bold;
+  border-top: 1.5px solid black;
+  border-right: 0px white;
+  border-bottom: 0px white;
+  border-left: 0px white;
+  border-style: solid;
+  border-radius: 11px;
+  /*box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 5px 0 rgba(0, 0, 0, 0.19);*/
+}
+.search-button:hover {
+  background-color: black;
+  color: white;
+}
+.results-body {
+  text-align: left;
+  padding-top: 50px;
+  padding-left: 100px;
+  padding-right: 100px;
+  padding-bottom: 50px;
+}
+.word {
+  font-weight: bold;
+  font-style: italic;
+  font-size: 22px;
+  padding-top: 30px;
+  padding-left: 7px;
+}
+.definition {
+  padding-top: 7px;
+  padding-left: 7px;
+}
+.user-name {
+  padding-top: 7px;
+  padding-left: 7px;
+}
 </style>
 
 <script>
@@ -165,7 +158,7 @@ export default {
       words: [],
       wordnikWords: [],
       search: "",
-      i: 0,
+      i: 0
     };
   },
   components: {
